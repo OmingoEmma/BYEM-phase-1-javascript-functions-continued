b3717a636fac989e378836644327ea150f8a6bb3
OmingoEmma <emmaomingo@gmail.com>
2024-03-29 15:57:57 +0300

    Complet lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+function saturdayFun(activity){
+    switch(activity){
+        case(activity === "skinnydip"):
+         return "This Saturday, I want to " + activity + "!";
+        default:
+         return "This Saturday, I want to " + defaultactivity + "!";      
+
+    }
+    
+}
+
+const defaultactivity = "roller-skate";  
+
+function  mondayWork(){
+    switch(activity){
+        case(activity == "cook"):
+        return "This Monday, I will" + activity;
+        default:
+            return "This Monday, I will go to the office";
+    }
+
+}
+
+function wrapAdjective(){
+    
+}
+
+
